0690865e4d959b9e654d5a2ae43e75828d942d5a
kwtuku <76758720+kwtuku@users.noreply.github.com>
2023-03-03 16:36:56 +0900

    chore: 型エラーを解消

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import { useState } from 'react';
 
-function Square({ value, onSquareClick }) {
+type Player = 'X' | 'O' | null;
+
+type SquareProps = {
+  value: Player;
+  onSquareClick: () => void;
+};
+
+function Square({ value, onSquareClick }: SquareProps) {
   return (
     <button className="square" onClick={onSquareClick}>
       {value}
@@ -8,8 +15,14 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-function Board({ xIsNext, squares, onPlay }) {
-  function handleClick(i) {
+type BoardProps = {
+  xIsNext: boolean;
+  squares: Player[];
+  onPlay: (nextSquares: Player[]) => void;
+};
+
+function Board({ xIsNext, squares, onPlay }: BoardProps) {
+  function handleClick(i: number) {
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
@@ -53,18 +66,18 @@ function Board({ xIsNext, squares, onPlay }) {
 }
 
 export default function Game() {
-  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [history, setHistory] = useState<Player[][]>([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
-  function handlePlay(nextSquares) {
+  function handlePlay(nextSquares: Player[]) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
   }
 
-  function jumpTo(nextMove) {
+  function jumpTo(nextMove: number) {
     setCurrentMove(nextMove);
   }
 
@@ -94,7 +107,7 @@ export default function Game() {
   );
 }
 
-function calculateWinner(squares) {
+function calculateWinner(squares: Player[]) {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
